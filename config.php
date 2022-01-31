3db559ddafe08ce3e6a171790550b24b440dad2c
ZobairSoodi <soodi.zobair.solicode@gmail.com>
2022-01-31 08:21:28 +0100

    ml

--- config.php
+++ config.php
@@ -6,11 +6,11 @@
     $trn4 = 100 * $tarifs["tr4"];
     $trn5 = 200 * $tarifs["tr5"];
 
-    function setTranche($Facture,$PU){
+    function setTranche($nbr,$Facture,$PU){
         global $tva;
         $Montant_HT = $Facture * $PU;
         $Montant_Taxes = $Montant_HT * $tva;
-        return "<tr><td>$Facture</td><td>$PU</td><td>" . $Montant_HT ."</td><td>$tva</td><td>" . $Montant_Taxes . "</td></tr>";
+        return "<tr><td>Tranche $nbr</td><td>$Facture</td><td>$PU</td><td>" . $Montant_HT ."</td><td>$tva</td><td>" . $Montant_Taxes . "</td></tr>";
     }
 
     
--- main.php
+++ main.php
@@ -6,8 +6,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
+<form action="main.php" method="post">
+        <input type="text" name="cons1" style="border: solid 2px rebeccapurple;">
+        <input type="text" name="cons2" style="border: solid 2px rebeccapurple;">
+        <input type="radio" name="cal" id="cal1" value="small"><label for="cal1">small</label>
+        <input type="radio" name="cal" id="cal2" value="medium"><label for="cal1">medium</label>
+        <input type="radio" name="cal" id="cal3" value="large"><label for="cal1">large</label>
+        <input type="submit">
+    </form>
 <?php
         // $above_210 = (210 * 0.9451);
         // $tva_210 = $above_210 * 0.14;
@@ -38,13 +48,13 @@
             if($price<=100){
                 global $total_initial;
                 $total_initial = $price * $tarifs["tr1"];
-                array_push($myArray, setTranche($price, $tarifs["tr1"]));
+                array_push($myArray, setTranche(1, $price, $tarifs["tr1"]));
             }
             else{
                 global $total_initial;
                 $total_initial = $trn1 + (($price - 100)*$tarifs["tr1"]);
-                array_push($myArray, setTranche(100, $tarifs["tr1"]),
-                    setTranche(($price-100), $tarifs["tr2"]));
+                array_push($myArray, setTranche(1, 100, $tarifs["tr1"]),
+                    setTranche(2, ($price-100), $tarifs["tr2"]));
                 
             }
         }
@@ -52,40 +62,41 @@
             if($price <= 210){
                 global $total_initial;
                 $total_initial = $price * $tarifs["tr3"];
-                array_push($myArray, setTranche($price, $tarifs["tr3"]));
+                array_push($myArray, setTranche(3, $price, $tarifs["tr3"]));
             }
             elseif ($price <= 310) {
                 global $total_initial;
                 $total_initial = $trn3 + (($price - 100)*$tarifs["tr4"]);
-                array_push($myArray, setTranche(210, $tarifs["tr3"]),
-                    setTranche(($price-210), $tarifs["tr4"])
+                array_push($myArray, setTranche(3, 210, $tarifs["tr3"]),
+                    setTranche(4, ($price-210), $tarifs["tr4"])
                 );
             }
             elseif($price <= 510){
                 global $total_initial;
                 $total_initial;
                 $total_initial = $trn3 + $trn4 + (($price - 310) * $tarifs["tr5"]);
-                array_push($myArray, setTranche(210, $tarifs["tr3"]),
-                    setTranche(100, $tarifs["tr4"]),
-                    setTranche(($price-310), $tarifs["tr5"])
+                array_push($myArray, setTranche(3, 210, $tarifs["tr3"]),
+                    setTranche(4, 100, $tarifs["tr4"]),
+                    setTranche(5, ($price-310), $tarifs["tr5"])
                 );
             }
             else{
                 global $total_initial;
                 $total_initial = $trn3 + $trn4 + $trn5 + (($price - 510) * $tarifs["tr6"]);
 
-                array_push($myArray, setTranche(210, $tarifs["tr3"]),
-                    setTranche(100, $tarifs["tr4"]),
-                    setTranche(200, $tarifs["tr5"]),
-                    setTranche(($price-510), $tarifs["tr6"])
+                array_push($myArray, setTranche(3, 210, $tarifs["tr3"]),
+                    setTranche(4, 100, $tarifs["tr4"]),
+                    setTranche(5, 200, $tarifs["tr5"]),
+                    setTranche(6, ($price-510), $tarifs["tr6"])
                 );
             }
         }
         $total_tva = $total_initial * $tva;
         
     ?>
-    <table border="1">
+    <table class="table table-striped">
         <thead>
+            <td></td>
             <td>Facturé</td>
             <td>P.U</td>
             <td>Montant HT</td>
@@ -100,22 +111,22 @@
                 if($cal == "small"){
                     global $total_initial;
                     $total_initial += 22.65;
-                    echo ("<tr><td></td><td></td><td>" . (22.65) . "</td><td>" . "0.14" ."</td><td>" . (22.65 * 0.14) . "</td></tr>");
-                    echo "<tr><td></td><td></td><td>" . ($total_initial + 22.65) . "</td><td></td><td>" . $total_tva . "</td></tr>";
+                    echo ("<tr><td>REDEVANCE FIXE ELECTRICITE</td><td></td><td>" . (22.65) . "</td><td>" . "0.14" ."</td><td>" . (22.65 * 0.14) . "</td><td></td></tr>");
+                    echo "<tr><td></td><td></td><td>" . ($total_initial + 22.65) . "</td><td></td><td>" . $total_tva . "</td><td></td></tr>";
                 }   
                 elseif($cal == "medium"){
                     global $total_initial;
                     $total_initial += 37.05;
-                    echo ("<tr><td></td><td></td><td>" . (37.05) . "</td><td>" . "0.14" ."</td><td>" . (37.05 * 0.14) . "</td></tr>");
-                    echo "<tr><td></td><td></td><td>" . ($total_initial + 37.05) . "</td><td></td><td>" . $total_tva . "</td></tr>";
+                    echo ("<tr><td>REDEVANCE FIXE ELECTRICITE</td><td></td><td>" . (37.05) . "</td><td>" . "0.14" ."</td><td>" . (37.05 * 0.14) . "</td></tr>");
+                    echo "<tr><td></td><td></td><td>" . ($total_initial + 37.05) . "</td><td></td><td>" . $total_tva . "</td><td></td></tr>";
                 }
                 elseif($cal == "large"){
                     global $total_initial;
                     $total_initial += 46.20;
-                    echo ("<tr><td></td><td></td><td>" . (46.20) . "</td><td>" . "0.14" ."</td><td>" . (46.20 * 0.14) . "</td></tr>");
-                    echo "<tr><td></td><td></td><td>" . ($total_initial + 46.20) . "</td><td></td><td>" . $total_tva . "</td></tr>";
+                    echo ("<tr><td>REDEVANCE FIXE ELECTRICITE</td><td></td><td>" . (46.20) . "</td><td>" . "0.14" ."</td><td>" . (46.20 * 0.14) . "</td><td></td></tr>");
+                    echo "<tr><td></td><td></td><td>" . ($total_initial + 46.20) . "</td><td></td><td>" . $total_tva . "</td><td></td></tr>";
                 }
-                echo "<tr><td></td><td></td><td></td><td>" . ($total_initial + $total_tva) . "</td></tr>";
+                echo "<tr><td></td><td></td><td></td><td>" . ($total_initial + $total_tva) . "</td><td></td><td></td></tr>";
             ?>
         </tbody>
     </table>

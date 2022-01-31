e5641e88236de8ade51b63ef8a842adfd43feb1b
ZobairSoodi <soodi.zobair.solicode@gmail.com>
2022-01-31 21:58:24 +0100

    almost finished

--- config.php
+++ config.php
@@ -1,6 +1,7 @@
 <?php 
     $tarifs = array("tr1"=>0.794, "tr2"=>0.883, "tr3"=>0.9451, "tr4"=> 1.0489, "tr5"=>1.2915, "tr6"=>1.4975);
     $tva = 0.14;
+    $timbre = 0.45;
     $trn1 = 100 * $tarifs["tr1"];
     $trn3 = 210 * $tarifs["tr3"];
     $trn4 = 100 * $tarifs["tr4"];
@@ -10,7 +11,20 @@
         global $tva;
         $Montant_HT = $Facture * $PU;
         $Montant_Taxes = $Montant_HT * $tva;
-        return "<tr><td>Tranche $nbr</td><td>$Facture</td><td>$PU</td><td>" . $Montant_HT ."</td><td>$tva</td><td>" . $Montant_Taxes . "</td></tr>";
+        return "<tr><td>Tranche $nbr</td><td>$Facture</td><td>$PU</td><td>" . $Montant_HT ."</td><td>$tva</td><td>" . $Montant_Taxes . "</td><td style='text-align:right;'> $nbr الشطر </td></tr>";
+    }
+
+    function setCal($cal){
+        global $tva;
+        return 
+        "<tr>
+            <td colspan='3' class='th'>REDEVANCE FIXE ELECTRICITE</td>
+            <td>$cal</td>
+            <td>$tva</td>
+            <td>". $cal * $tva . "</td>
+            <td style='text-align:right;' class='th'>إثاوة ثابتة الكهرباء</td>
+        </tr>
+        ";
     }
 
     
--- main.php
+++ main.php
@@ -7,7 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <!-- CSS only -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 <form action="main.php" method="post">
@@ -52,7 +53,7 @@
             }
             else{
                 global $total_initial;
-                $total_initial = $trn1 + (($price - 100)*$tarifs["tr1"]);
+                $total_initial = $trn1 + (($price - 100)*$tarifs["tr2"]);
                 array_push($myArray, setTranche(1, 100, $tarifs["tr1"]),
                     setTranche(2, ($price-100), $tarifs["tr2"]));
                 
@@ -94,16 +95,29 @@
         $total_tva = $total_initial * $tva;
         
     ?>
-    <table class="table table-striped">
+    <div id='cons'>
+        <div>Ancien index : <?php echo $cons1?> kWh</div>
+        <div>Nouvel index : <?php echo $cons2?> kWh</div>
+        <div>Consommation : <?php echo $price?> kWh</div>
+    </div>
+    <table class="table table-striped table-dark">
         <thead>
-            <td></td>
-            <td>Facturé</td>
-            <td>P.U</td>
-            <td>Montant HT</td>
-            <td>Taux TVA</td>
-            <td>Montant Taxes</td>
+            <tr class="th">
+                <td width="20%"></td>
+                <td>Facturé</td>
+                <td>P.U</td>
+                <td>Montant HT</td>
+                <td>Taux TVA</td>
+                <td>Montant Taxes</td>
+                <td></td>
+            </tr>
+            
         </thead>
         <tbody>
+            <tr>
+                <td class="th" colspan="3" style="">CONSOMMATION ELECTRICITE</td>
+                <td class='th' colspan="4" style="text-align:right;">إستھلاك الكھرباء </td>
+            </tr>
             <?php 
                 foreach ($myArray as $i) {
                     echo $i;
@@ -111,22 +125,46 @@
                 if($cal == "small"){
                     global $total_initial;
                     $total_initial += 22.65;
-                    echo ("<tr><td>REDEVANCE FIXE ELECTRICITE</td><td></td><td>" . (22.65) . "</td><td>" . "0.14" ."</td><td>" . (22.65 * 0.14) . "</td><td></td></tr>");
-                    echo "<tr><td></td><td></td><td>" . ($total_initial + 22.65) . "</td><td></td><td>" . $total_tva . "</td><td></td></tr>";
+                    $total_tva += 22.65 * 0.14;
+                    echo setCal(22.65);
                 }   
                 elseif($cal == "medium"){
                     global $total_initial;
                     $total_initial += 37.05;
-                    echo ("<tr><td>REDEVANCE FIXE ELECTRICITE</td><td></td><td>" . (37.05) . "</td><td>" . "0.14" ."</td><td>" . (37.05 * 0.14) . "</td></tr>");
-                    echo "<tr><td></td><td></td><td>" . ($total_initial + 37.05) . "</td><td></td><td>" . $total_tva . "</td><td></td></tr>";
+                    $total_tva += 37.05 * 0.14;
+                    echo setCal(37.05);
                 }
                 elseif($cal == "large"){
                     global $total_initial;
                     $total_initial += 46.20;
-                    echo ("<tr><td>REDEVANCE FIXE ELECTRICITE</td><td></td><td>" . (46.20) . "</td><td>" . "0.14" ."</td><td>" . (46.20 * 0.14) . "</td><td></td></tr>");
-                    echo "<tr><td></td><td></td><td>" . ($total_initial + 46.20) . "</td><td></td><td>" . $total_tva . "</td><td></td></tr>";
+                    $total_tva += 46.20 * 0.14;
+                    echo setCal(46.20);
                 }
-                echo "<tr><td></td><td></td><td></td><td>" . ($total_initial + $total_tva) . "</td><td></td><td></td></tr>";
+                echo "<tr>
+                        <td colspan='3' class='th'>TAXES POUR LE COMPTE DE L’ETAT</td>
+                        <td colspan='4' class='th' style='text-align:right;' >الرسوم المؤداة لفائدة الدولة</td>
+                    </tr>";
+                echo "<tr>
+                        <td colspan='5'>TOTAL TVA</td>
+                        <td>$total_tva</td>
+                        <td style='text-align:right' colspan='5'>مجموع ض.ق.م</td>
+                    </tr>";
+                echo "<tr>
+                        <td colspan='5'>Timbre</td>
+                        <td>$timbre</td>
+                        <td style='text-align:right' colspan='5'>الطابع</td>
+                    </tr>";
+                echo "<tr>
+                        <td colspan='3' class='th'>SOUS-TOTAL</td>
+                        <td class='th'>$total_initial</td><td></td>
+                        <td class='th'>" . ($total_tva + $timbre) . "</td>
+                        <td style='text-align:right;' class='th'>المجموع الجزئي</td>
+                    </tr>";
+                echo "<tr>
+                        <td colspan='3' class='th'>TOTAL ÉLECTRICITÉ</td>
+                        <td  style='text-align:center;' class='th'>" . ($total_initial + $total_tva + $timbre) . "</td>
+                        <td style='text-align:right;' colspan='3' class='th'>مجموع الكهرباء</td>
+                    </tr>"
             ?>
         </tbody>
     </table>

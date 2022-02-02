86fe397759c804fbcf62ba90becb41da7d9cb7f0
BouzineAicha <92594958+BouzineAicha@users.noreply.github.com>
2022-02-02 12:55:45 +0100

    hi

--- test.php
+++ test.php
@@ -33,16 +33,19 @@ $trnList = [
 ];
 
 $tva = 0.14;
+$TVA = "14%";
 $SMALL =22.65;
 $MEDIUM =37.05;
 $LARGE=46.20;
+ 
 
 $index1 = $_POST["cons1"];
 $index2 = $_POST["cons2"];
 $cons = $index2 - $index1;
 
-function show($facture,$PU){
+function show( $nbr,$facture,$PU){
     $montantHT = $facture * $PU;
+    global $TVA;
     global $tva;
     $montantTaxes = $montantHT * $tva;
     echo "<tr>
@@ -50,77 +53,122 @@ function show($facture,$PU){
     <td>$facture</td>
     <td>$PU</td>
     <td>$montantHT</td>
-    <td>$tva</td>
+    <td>$TVA</td>
     <td>$montantTaxes</td>
-    <td>$nbr الشطر</td>
+    <td colspan='4' class='th' style='text-align:right;' >$nbr الشطر</td>
 </tr>";
 }
+$calc = array("smallCalibr"=>22.65,"mediumCalibr"=>37.05,"largeCalibr"=>46.20);
 
 ?>
 
-<table class="table">
+<table class="table" >
     <thead>
-        <td>Facture</td>
-        <td>PU</td>
-        <td>Montant HT</td>
-        <td>TAUX TVA</td>
-        <td>Montant Taxes</td>
+        <td></td>
+        <td>Facture <br> مفوتر</td>
+        <td>PU <br>س.و</td>
+        <td>Montant HT <br>المبلغ د.إ.ر</td>
+        <td>TAUX TVA <br>ض.ق.م </td>
+        <td>Montant Taxes<br>مبلغ الرسوم </td>
+        <td></td>
     </thead>
     <tbody>
         
         <?php
+        
+        echo "<tr><th  colspan='3' class='th'>E CONSOMMATIO</th><th  colspan='4' class='th' style='text-align:right;'>إستھلاك الكھرباء </th></tr>";
             if($cons <= 150){
                 if($cons <= 100){
-                    show($cons, $trnList[0]->PU);
+                    show(1,$cons, $trnList[0]->PU);
                     $montantHT =$cons*$trnList[0]->PU;
 
                 }
                 else{
-                    show(($cons - 100), $trnList[0]->PU);
-                    show($cons, $trnList[1]->PU);
+                    show(1,($cons - 100), $trnList[0]->PU);
+                    show(2,$cons, $trnList[1]->PU);
                     $montantHT =$cons*$trnList[1]->PU;
                 }
             }
             else{
                 if($cons <= 210){
-                    show($cons, $trnList[2]->PU);
+                    show(3,$cons, $trnList[2]->PU);
                     $montantHT =$cons*$trnList[2]->PU;
                 }
                 elseif($cons <= 310){
-                    show($cons, $trnList[3]->PU);
+                    show(4,$cons, $trnList[3]->PU);
                     $montantHT =$cons*$trnList[3]->PU;
                 }
                 elseif($cons <= 510){
-                    show($cons, $trnList[4]->PU);
+                    show(5,$cons, $trnList[4]->PU);
                     $montantHT =$cons*$trnList[4]->PU;
                 }
                 elseif($cons <= 310){
-                    show($cons, $trnList[5]->PU);
+                    show(6,$cons, $trnList[5]->PU);
                     $montantHT =$cons*$trnList[5]->PU;
                 }
             }
             $cal = $_POST["cal"];
-            if($cal ="small"){
-               
-                $montantHT += $SMALL;
+            if($cal =="small"){
+               $calibre = $calc ["smallCalibr"];
+                 $montantHT += $SMALL;
             }
-                elseif ($cal ="medium"){
-                   
-                   $montantHT += $MEDIUM ;
+                elseif ($cal =="medium"){
+                    $calibre = $calc ["mediumCalibr"];
+                      $montantHT += $MEDIUM ;
                   
                 }
                 else{
-                    
+                    $calibre = $calc ["largeCalibr"];
                     $montantHT += $LARGE;  
                 }
-            
-                echo "<tr> <td>REDEVANCE FIXE ELECTRICITE</td>  
-                <td> $SMALL </td > <td>الكھرباء ثابتة إ </td></tr>";
+                $tv= $MEDIUM *$tva; 
+                echo "<tr colspan='3' class='th'> <th>REDEVANCE FIXE ELECTRICITE</th>  
+                 <td></td> <td></td><td>   $calibre </td ><td>$TVA</td><td> $tv</td> <th colspan='4' class='th' style='text-align:right;'>إثاوة الكھرباء ثابتة</th></tr>";
                 echo "<tr>
-                        <td colspan='3' class='th'>TAXES POUR LE COMPTE DE L’ETAT</td>
-                        <td colspan='4' class='th' style='text-align:right;' >الرسوم المؤداة لفائدة الدولة</td>
+                        <th colspan='3' class='th'>TAXES POUR LE COMPTE DE L’ETAT</th>
+                        <th colspan='4' class='th' style='text-align:right;' >الرسوم المؤداة لفائدة الدولة</th>
                     </tr>";
         ?>
+        <tr>
+            <td>TVA TOTAL </td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+           
+            <td><?php echo $montantHT;?></td>
+            <td colspan='4' class='th' style='text-align:right;'> مجموع ض.ق.م </td>
+        </tr>
+        <tr>
+            <td> TIMBRE </td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            
+            <td>0,45</td>
+            <td colspan='4' class='th' style='text-align:right;'> الطابع</td>
+        </tr>
+    
+        <tr>
+            <td>SOUS-TOTAL </td>
+            <td></td>
+            <td></td>
+            <td><?php echo $montantHT  + $calibre; ?></td>
+            <td></td>
+            
+            <td>0,45</td>
+            <td colspan='4' class='th' style='text-align:right;'> الجزئي المجموع</td>
+        </tr>
+        <tr>
+            <td>TOTAL ÉLECTRICITÉ </td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td colspan='4' class='th' style='text-align:right;' >مجموع الكھرباء </td>
+        </tr>
     
 
 
